0c5e5593889cbbe452c450d59680330c88e85900
Ryan Echols <rkechols21@gmail.com>
2020-11-09 11:27:19 -0700

    fixed and ran script to split original csv into 5 individual txt files

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
 UTF_8 = "utf8"
+TXT_FILE_NAME_F = "./data/tripadvisor_{}-star.txt"
--- split_csv_to_txt.py
+++ split_csv_to_txt.py
@@ -1,10 +1,9 @@
 import os
 import re
-from constants import UTF_8
+from constants import TXT_FILE_NAME_F, UTF_8
 
 
 CSV_FILE_NAME = "./data/tripadvisor_hotel_reviews.csv"
-TXT_FILE_NAME_F = "./data/tripadvisor_{}-star.txt"
 DEAD_LINES_FILE_NAME = "dead_lines.csv"
 CSV_LINE_RE = re.compile(r"\"(.*)\",([0-9])")
 
@@ -26,7 +25,7 @@ if __name__ == "__main__":
 				if line_num == 1:  # skip the header row
 					continue
 				line = line_.strip()
-				match = CSV_LINE_RE.fullmatch(line_)
+				match = CSV_LINE_RE.match(line_)
 				if match is None:
 					print(f"{line_num},{line}", file=open(DEAD_LINES_FILE_NAME, "a", encoding=UTF_8))
 				text = match.group(1)
