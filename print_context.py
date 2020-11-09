bfcf8253a04ad08921656f4d30144d0fcd5ff4d2
Ryan Echols <rkechols21@gmail.com>
2020-11-09 15:56:31 -0700

    context searcher complete

--- print_context.py
+++ print_context.py
@@ -1,15 +1,33 @@
-from tokenize_english import get_all_tokenized_ratings
+import sys
+import multiprocessing as mp
+import os
+from tokenize_english import read_tks_file
+from util import count_lemma_in_tokens, get_tokenized_file_name, tokens_to_str, UTF_8
 
 
-CONTEXT_FILE_NAME_F = "./data/contexts/{}_context_{}.txt"
+CONTEXTS_DIR = "./data/contexts/"
+CONTEXT_FILE_NAME_F = CONTEXTS_DIR + "{}_context_{}.txt"
 
 
-def get_context_file_name(word: str, rating_level: int) -> str:
-	return CONTEXT_FILE_NAME_F.format(word, rating_level)
+def get_context_file_name(lemma: str, rating_level: int) -> str:
+	return CONTEXT_FILE_NAME_F.format(lemma, rating_level)
+
+
+def find_in_context_all(lemma: str) -> str:
+	for i in range(5):
+		rating_level = i + 1
+		doc = read_tks_file(get_tokenized_file_name(rating_level))
+		with open(get_context_file_name(lemma, rating_level), "w", encoding=UTF_8) as context_file:
+			for line_num, tokenized_review in enumerate(doc, start=1):
+				if count_lemma_in_tokens(lemma, tokenized_review) != 0:
+					print(f"{line_num}:\t{tokens_to_str(tokenized_review)}", file=context_file)
+	return lemma
 
 
 if __name__ == "__main__":
-	# tokenize files
-	docs = get_all_tokenized_ratings()
-	# find tokens in question
-	# TODO
+	if not os.path.exists(CONTEXTS_DIR):
+		os.mkdir(CONTEXTS_DIR)
+	words = sys.argv[1:]
+	with mp.Pool() as pool:
+		for word in pool.imap_unordered(find_in_context_all, words):
+			print(f"search for \"{word}\" complete")
--- util.py
+++ util.py
@@ -1,3 +1,7 @@
+from typing import List
+from simple_token import SimpleToken
+
+
 UTF_8 = "utf8"
 SPACY_ENGLISH_NAME = "en"
 
@@ -14,3 +18,15 @@ def get_rating_level_file_name(rating_level: int) -> str:
 
 def get_tokenized_file_name(rating_level: int) -> str:
 	return TOKENIZED_FILE_NAME_F.format(rating_level)
+
+
+def count_lemma_in_tokens(lemma: str, tokens: List[SimpleToken]) -> int:
+	count = 0
+	for t in tokens:
+		if t.lemma == lemma:
+			count += 1
+	return count
+
+
+def tokens_to_str(tokens: List[SimpleToken]) -> str:
+	return " ".join((t.text for t in tokens))
