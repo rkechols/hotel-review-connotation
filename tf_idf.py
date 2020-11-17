ac7d3022085d526d229e6173ad2599814606d50a
Ryan Echols <rkechols21@gmail.com>
2020-11-16 18:17:39 -0700

    finished initial run of tf-idf

--- tf_idf.py
+++ tf_idf.py
@@ -1,9 +1,10 @@
-from typing import Dict, List, Tuple
-from gensim.models import TfidfModel
+import math
+import os
+from typing import Dict, List, Set, Tuple
+# from gensim.models import TfidfModel
 from gensim.corpora import Dictionary
-from os import path
 from tokenize_english import read_tks_file_flat
-from util import get_tokenized_file_name
+from util import get_scores_file_name, get_tokenized_file_name, SCORES_DIR, UTF_8
 
 
 Bow = List[Tuple[int, int]]
@@ -28,7 +29,7 @@ def get_document_token_counts(documents: Dict[int, List[str]]) -> Dict[int, int]
 
 
 def get_dictionary(documents: Dict[int, List[str]]) -> Dictionary:
-	if path.exists(DICTIONARY_FILE_NAME):
+	if os.path.exists(DICTIONARY_FILE_NAME):
 		print(f"loading dictionary from {DICTIONARY_FILE_NAME}")
 		gensim_dict = Dictionary.load(DICTIONARY_FILE_NAME)
 	else:
@@ -48,17 +49,76 @@ def get_corpus(documents: Dict[int, List[str]], dictionary: Dictionary) -> Dict[
 	return corpus
 
 
+def get_bags_of_word_ids(corpus: Dict[int, Bow]) -> Dict[int, Set[int]]:
+	to_return = dict()
+	for rating_level, bow in corpus.items():
+		this_set = set()
+		for word_id, _ in bow:
+			this_set.add(word_id)
+		to_return[rating_level] = this_set
+	return to_return
+
+
+class ScoreInfo:
+	def __init__(self, word_id: int, tf_idf: float, count_in_doc: int, present_in_doc_count: int):
+		self.word_id = word_id
+		self.tf_idf = tf_idf
+		self.count_in_doc = count_in_doc
+		self.present_in_doc_count = present_in_doc_count
+
+	def __lt__(self, other):
+		if not isinstance(other, ScoreInfo):
+			raise ValueError(f"cannot be compared with non-{self.__class__.__name__} object; received object of class {other.__class__.__name__}")
+		return self.tf_idf < other.tf_idf
+
+
+def get_tf_idf_scores(corpus: Dict[int, Bow], token_counts: Dict[int, int], bags_of_ids: Dict[int, Set[int]]) -> Dict[int, List[ScoreInfo]]:
+	doc_count = len(bags_of_ids)
+	to_return = dict()
+	for rating_level, bow in corpus.items():
+		doc_size = token_counts[rating_level]
+		these_scores = list()
+		for word_id, count in bow:
+			tf = count / doc_size
+			present_in_doc_count = 0
+			for bag_of_ids in bags_of_ids.values():
+				if word_id in bag_of_ids:
+					present_in_doc_count += 1
+			idf = math.log(doc_count / present_in_doc_count)
+			these_scores.append(ScoreInfo(word_id, tf * idf, count, present_in_doc_count))
+		these_scores.sort(reverse=True)  # sort by score, big to small
+		to_return[rating_level] = these_scores
+	return to_return
+
+
+def save_scores_to_files(dictionary: Dictionary, scores: Dict[int, List[ScoreInfo]]):
+	print(f"saving scores to files in {SCORES_DIR}")
+	if not os.path.isdir(SCORES_DIR):
+		os.mkdir(SCORES_DIR)
+	for rating_level, scores_list in scores.items():
+		with open(get_scores_file_name(rating_level), "w", encoding=UTF_8) as scores_file:
+			# print the header
+			print("lemma,score,count in doc,in how many docs", file=scores_file)
+			for s in scores_list:
+				print(f"\"{dictionary[s.word_id]}\",{s.tf_idf},{s.count_in_doc},{s.present_in_doc_count}", file=scores_file)
+
+
 if __name__ == "__main__":
 	docs = get_lemmatized_documents()
-	token_counts = get_document_token_counts(docs)
+	t_counts = get_document_token_counts(docs)
 	print("----------")
-	print(f"total token count: {sum(token_counts.values())}")
+	print(f"total token count: {sum(t_counts.values())}")
 	print("----------")
 	d = get_dictionary(docs)
 	corp = get_corpus(docs, d)
 	print("----------")
 	print(f"total type count: {len(d)}")
 	print("----------")
-	for r_level, bow in corp.items():
-		print(f"types in all {r_level}-star reviews: {len(bow)}")
+	for r_level, bag in corp.items():
+		print(f"types in all {r_level}-star reviews: {len(bag)}")
 	print("----------")
+	print("calculating tf-idf scores...")
+	bags_of_just_ids = get_bags_of_word_ids(corp)
+	all_scores = get_tf_idf_scores(corp, t_counts, bags_of_just_ids)
+	save_scores_to_files(d, all_scores)
+	print("done!")
--- util.py
+++ util.py
@@ -11,6 +11,9 @@ TXT_FILE_NAME_F = SPLIT_DIR + "tripadvisor_{}-star.txt"
 TOKENIZED_DIR = "./data/tokenized/"
 TOKENIZED_FILE_NAME_F = TOKENIZED_DIR + "{}-star-tokenized.tks"
 
+SCORES_DIR = "./data/scores/"
+SCORES_FILE_NAME_F = SCORES_DIR + "{}-star_scores.csv"
+
 
 def get_rating_level_file_name(rating_level: int) -> str:
 	return TXT_FILE_NAME_F.format(rating_level)
@@ -18,6 +21,10 @@ def get_rating_level_file_name(rating_level: int) -> str:
 
 def get_tokenized_file_name(rating_level: int) -> str:
 	return TOKENIZED_FILE_NAME_F.format(rating_level)
+
+
+def get_scores_file_name(rating_level: int) -> str:
+	return SCORES_FILE_NAME_F.format(rating_level)
 
 
 def count_lemma_in_tokens(lemma: str, tokens: List[SimpleToken]) -> int:
